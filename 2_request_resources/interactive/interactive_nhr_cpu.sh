cc519409d4eb841a521728cd4faa5f227f556ec9
GOESTERN-1026576 <u18550@glogin12.usr.hpc.gwdg.de>
2025-07-23 15:47:42 +0200

    update interactive again

--- 2_request_resources/interactive/interactive_nhr_cpu.sh
+++ 2_request_resources/interactive/interactive_nhr_cpu.sh
@@ -3,17 +3,12 @@
 #SBATCH -c 8 # number of CPU cores to allocate
 #SBATCH -t 0-01:00:00 # time limit in days-hours:minutes:seconds
 #SBATCH -A scc_uwvn_kneib # name of the compute project you are in
-#SBATCH --output=/dev/null # no output log is generated (comment out by adding an extra # to the beginning of the line)
-#SBATCH --error=/dev/null # no output log is generated (comment out by adding an extra # to the beginning of the line)
+#SBATCH --output=/dev/null # no output log is generated
+#SBATCH --error=/dev/null # no error log is generated
 
 ############## OPTIONAL SLURM ARGS
-### 1. 
 # put "#SBATCH -C inet as slurm option" and comment out the below lines for internet access on compute node (e.g. for downloading/uploading stuff)
 # export HTTPS_PROXY="http://www-cache.gwdg.de:3128"
 # export HTTP_PROXY="http://www-cache.gwdg.de:3128"
-### 2.
-# use these two lines instead of the ones above to log output and errors to a file
-#SBATCH --output=slurm_logs/slurm_log_%j.log # use this line instead of the one above to log output to a file
-#SBATCH --error=slurm_logs/slurm_log_%j.log # use this line instead of the one above to log errors to a file
 
 sleep infinity # make sure the job does not exit immediately so that you can ssh to the granted compute node
